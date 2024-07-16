b127b2ae7afdf6110f4ffc1facb000cdeedf9194
peter <112580184+petyahyria@users.noreply.github.com>
2024-07-16 13:27:29 +0300

    Add function getHumanChoice in script.js

--- script.js
+++ script.js
@@ -5,3 +5,12 @@ let getComputerChoice = () => {
     let computerChoice = Math.floor(Math.random() * 3);
     return choices[computerChoice];
 }
+
+/* Human choice */
+
+let getHumanChoice = () => {
+    let humanChoice = prompt("Type 'Rock', 'Paper' or 'Scissors' (without quotes)");
+    return humanChoice; 
+}
+
+console.log(getHumanChoice());
