62acc721716e85beda77a913fdd405350e000905
peter <112580184+petyahyria@users.noreply.github.com>
2024-07-16 14:08:07 +0300

    Add playing round feature in script.js

--- script.js
+++ script.js
@@ -21,3 +21,51 @@ let getHumanChoice = () => {
 
 let humanScore = 0;
 let computerScore = 0;
+
+/* Play Round */
+
+let playRound = (computerChoice, humanChoice) => {
+    if(computerChoice === "Rock"){
+        switch (humanChoice) {
+            case "Rock":
+                console.log("Tie! Nobody gets a point")
+                break;
+            case "Paper":
+                console.log("You win this round! You get a point!")
+                humanScore++;
+                break;
+            case "Scissors":
+                console.log("You lose this round! Computer gets a point")
+                computerScore++;
+                break;
+        }
+    }else if(computerChoice === "Paper"){
+        switch (humanChoice) {
+            case "Paper":
+                console.log("Tie! Nobody gets a point")
+                break;
+            case "Scissors":
+                console.log("You win this round! You get a point!")
+                humanScore++;
+                break;
+            case "Rock":
+                console.log("You lose this round! Computer gets a point")
+                computerScore++;
+                break;
+        }
+    }else {
+        switch (humanChoice) {
+            case "Scissors":
+                console.log("Tie! Nobody gets a point")
+                break;
+            case "Rock":
+                console.log("You win this round! You get a point!")
+                humanScore++;
+                break;
+            case "Paper":
+                console.log("You lose this round! Computer gets a point")
+                computerScore++;
+                break;
+        }
+    }
+}
