a97e7e94cd3c7e2e849427d55d982233dfa87fdb
peter <112580184+petyahyria@users.noreply.github.com>
2024-07-16 13:14:46 +0300

    Add function getComputerChoice() in script.js
    Because of we play against a computer and computer must choose random
    choice.

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-console.log("Hello World");
+/* Computer Choice */
+
+let getComputerChoice = () => {
+    let choices = ["Rock", "Paper", "Scissors"];
+    let computerChoice = Math.floor(Math.random() * 3);
+    return choices[computerChoice];
+}
